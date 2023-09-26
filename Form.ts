094e009979b5228a7fcde81ce1fbc148abb8e216
SviatoslavB <91957024+SviatoslavB@users.noreply.github.com>
2023-09-26 12:57:19 +0200

    2 files modified

--- Form.ts
+++ Form.ts
@@ -2,10 +2,11 @@ import { Dreieck } from './Dreieck';
 import { Kreis } from './Kreis';
 import { Rechteck } from './Rechteck';
 
-let kreis: Kreis;
-let rechteck: Rechteck;
+//let kreis: Kreis;
+//let rechteck: Rechteck;
 
-export abstract class Form {
+//export dammit es auserhalb verfügbar ist
+export  abstract class Form {
   farbe: String;
 
   constructor(farbe: String) {
@@ -16,10 +17,3 @@ export abstract class Form {
     return 0;
   }
 }
-const weissesKreis = new Kreis('weis', 4);
-const grünesRechteck = new Rechteck('grün', 6, 8);
-const orangesDreieck = new Dreieck('orange', 6, 5);
-
-console.log('Fläche des weißes Kreises', weissesKreis.berechneFlaeche(), weissesKreis.toString());
-console.log('Fläche des grünes Rechtecks', grünesRechteck.berechneFlaeche(), grünesRechteck.toString());
-console.log('Fläche des oranges Dreieck', orangesDreieck.berechneFlaeche(), orangesDreieck.toString());
--- index.ts
+++ index.ts
@@ -1,6 +1,17 @@
 // Import stylesheets
+import { Dreieck } from './Dreieck';
+import { Kreis } from './Kreis';
+import { Rechteck } from './Rechteck';
 import './style.css';
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
 appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+const weissesKreis = new Kreis('weis', 4);
+const grünesRechteck = new Rechteck('grün', 6, 8);
+const orangesDreieck = new Dreieck('orange', 6, 5);
+
+console.log('Fläche des weißes Kreises', weissesKreis.berechneFlaeche(), weissesKreis.toString());
+console.log('Fläche des grünes Rechtecks', grünesRechteck.berechneFlaeche(), grünesRechteck.toString());
+console.log('Fläche des oranges Dreieck', orangesDreieck.berechneFlaeche(), orangesDreieck.toString());
